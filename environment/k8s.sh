672f5a1dc7e7575cf6a7f07fa6f8d4f41a63ae1c
a595859893 <595859893@qq.com>
2020-03-10 23:25:44 +0800

    add ali mirror

--- environment/k8s.sh
+++ environment/k8s.sh
@@ -3,5 +3,6 @@ sh ./init.sh
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
    && sudo install minikube-linux-amd64 /usr/local/bin/minikube
-sudo minikube start --driver=none
-sudo minikube config set driver none
+sudo minikube start --image-mirror-country cn \
+    --iso-url=https://kubernetes.oss-cn-hangzhou.aliyuncs.com/minikube/iso/minikube-v1.7.3.iso \
+    --registry-mirror=https://xxxxxx.mirror.aliyuncs.com
