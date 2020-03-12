eb0efc689a80465308220802da194d5fe676f589
翁天俊 <595859893@qq.com>
2020-03-12 11:53:44 +0800

    add sudo
    sometime without sudo will get permission denied when download whlie sometime not
    I don‘t know why
    just add to ensure it is good

--- environment/dev.sh
+++ environment/dev.sh
@@ -1,12 +1,12 @@
 # nodejs
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g cnpm --registry=https://registry.npm.taobao.org
 # python3
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 # golang
-wget https://studygolang.com/dl/golang/go1.13.8.linux-amd64.tar.gz
+sudo wget https://studygolang.com/dl/golang/go1.13.8.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.13.8.linux-amd64.tar.gz
 sudo rm go1.13.8.linux-amd64.tar.gz
 sudo echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
